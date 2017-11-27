d6c71316ace0896d96fa2f6ae1b56a6e2b7de4e5
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2017-11-27 13:40:32 +0100

    Add followComponent and optimize some paths

---
+++ app/components/user/follow/followComponent.js
@@ -0,0 +1,124 @@
+angular.module('lejour.user.follow', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/user/follow', {
+        templateUrl: '/app/components/user/follow/followComponent.html',
+        controller: 'followController',
+        resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          // Auth refers to our $firebaseAuth wrapper in the factory below
+          "currentAuth": function (Auth) {
+            // $waitForSignIn returns a promise so the resolve waits for it to complete
+            return Auth.$requireSignInWithRole();
+          }
+        }
+      });
+  })
+  .controller('followController', function ($rootScope, $scope, $location, $routeParams, $mdToast, currentAuth, Firestore, $q) {
+    $rootScope.title = 'Abonnements';
+
+    $scope.role = currentAuth.role;
+
+    $scope.confirmedConnectionsText;
+    $scope.unconfirmedConnectionsText;
+    $scope.confirmedConnections = [];
+    $scope.unconfirmedConnections = [];
+
+    if ($scope.role === "apprentice") {
+      $scope.unconfirmedConnectionsText = "Unbestätigte Lehrmeister";
+      $scope.confirmedConnectionsText = "Bestätigte Lehrmeister";
+
+      Firestore.$getConfirmedMentorsFromApprenticeWithEmail(currentAuth.email)
+        .then(function (querySnapshot) {
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach(function (doc) {
+              $scope.$apply(function () {
+                $scope.confirmedConnections.push(doc.data());
+              });
+            });
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+          $mdToast.showSimple('Konnte nicht Lehrmeister aus der Datenbank holen. Versuchen Sie es später noch einmal');
+        });
+      Firestore.$getUnconfirmedMentorsFromApprenticeWithEmail(currentAuth.email)
+        .then(function (querySnapshot) {
+          console.log(querySnapshot.empty);
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach(function (doc) {
+              $scope.$apply(function () {
+                $scope.unconfirmedConnections.push(doc.data());
+              });
+            });
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+          $mdToast.showSimple('Konnte nicht Lehrmeister aus der Datenbank holen. Versuchen Sie es später noch einmal');
+        });
+
+      $scope.accept = function (mentorEmail) {
+        Firestore.$acceptMentorWithEmailFromApprenticeWithEmail(mentorEmail, currentAuth.email)
+          .then(function () {
+            $mdToast.showSimple('Annahme erfolgreich!');
+          }).catch(function () {
+          $mdToast.showSimple('Annahme fehlgeschlagen!');
+        });
+      }
+    } else {
+      $scope.unconfirmedConnectionsText = "Unbestätigte Lehrlinge";
+      $scope.confirmedConnectionsText = "Bestätigte Lehrlinge";
+
+      $scope.selectedApprentice = null;
+
+      Firestore.$getConfirmedApprenticesFromMentorWithEmail(currentAuth.email).then(function (confirmedApprentices) {
+        $scope.confirmedConnections = confirmedApprentices;
+      });
+
+      Firestore.$getUnconfirmedApprenticesFromMentorWithEmail(currentAuth.email).then(function (unconfirmedApprentices) {
+        $scope.unconfirmedConnections = unconfirmedApprentices;
+      });
+
+      $scope.querySearch = function (query) {
+        var deferred = $q.defer();
+        var apprentices = [];
+        if (query) {
+          Firestore.$getApprenticesWithEmail(query).then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+              apprentices.push(doc.data());
+            });
+            deferred.resolve(apprentices);
+          });
+        } else {
+          Firestore.$getFirst50Apprentices().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+              apprentices.push(doc.data());
+            });
+            deferred.resolve(apprentices);
+          });
+        }
+        return deferred.promise;
+      };
+
+      $scope.selectedItemChange = function (item) {
+        if (typeof item === 'undefined') {
+          $scope.selectedApprentice = null;
+        } else {
+          $scope.selectedApprentice = item;
+          Firestore.$getFollowStatusOfMentorsWithEmailFromApprenticeWithEmail(currentAuth.email, $scope.selectedApprentice.email).then(function (result) {
+            $scope.selectedApprentice.status = result.status;
+          });
+        }
+      };
+
+      $scope.follow = function () {
+        Firestore.$followApprenticeWithEmailFromMentorWithEmail(currentAuth.email, $scope.selectedApprentice.email)
+          .then(function () {
+            $mdToast.showSimple('Anfrage erfolgreich!');
+          }).catch(function () {
+          $mdToast.showSimple('Anfrage fehlgeschlagen!');
+        });
+      }
+    }
+  });
--- app/shared/factories/firestore.js
+++ app/shared/factories/firestore.js
@@ -3,14 +3,14 @@ angular.module('lejour.firebase.firestore', [
   'firebase'
 ])
 
-  .factory('Firestore', function () {
+  .factory('Firestore', function ($q) {
     var firestore = firebase.firestore();
 
     firestore.$getUserDatabase = function () {
       return firestore.collection('users');
     };
 
-    firestore.$createUserWithEmailAndRole = function(email, role) {
+    firestore.$createUserWithEmailAndRole = function (email, role) {
       return firestore.$getUserDatabase().add({
         email: email,
         role: role
@@ -21,19 +21,144 @@ angular.module('lejour.firebase.firestore', [
       return firestore.$getUserDatabase().doc(id);
     };
 
+    firestore.$getUserWithEmail = function (email) {
+      var defer = $q.defer();
+      firestore.$getUserDatabase().where("email", "==", email).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          firestore.$getUserWithId(doc.id).get().then(function (doc) {
+            defer.resolve(doc.data());
+          });
+        });
+      });
+      return defer.promise;
+    };
+
     firestore.$deleteUserWithEmail = function (email) {
-      return firestore.$getUserDatabase().where("email", "==", email).get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
+      return firestore.$getUserDatabase().where("email", "==", email).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
           firestore.$getUserWithId(doc.id).delete();
         });
       });
     };
 
-    firestore.$getJournalDatabase = function() {
+    firestore.$getApprentices = function () {
+      return firestore.$getUserDatabase().where("role", "==", "apprentice").get();
+    };
+
+    firestore.$getApprenticesWithEmail = function (email) {
+      var emailFrontCode = email.slice(0, email.length - 1);
+      var emailEndCode = email.slice(email.length - 1, email.length);
+
+      var startCode = email;
+      var endCode = emailFrontCode + String.fromCharCode(emailEndCode.charCodeAt(0) + 1);
+
+      return firestore.$getUserDatabase().where("role", "==", "apprentice")
+        .where("email", ">=", startCode)
+        .where("email", "<", endCode)
+        .get();
+    };
+
+    firestore.$getFirst50Apprentices = function () {
+      return firestore.$getUserDatabase().where("role", "==", "apprentice")
+        .limit(50)
+        .get();
+    };
+
+    firestore.$getFollowStatusOfMentorsWithEmailFromApprenticeWithEmail = function (mentorEmail, apprenticeEmail) {
+      var defer = $q.defer();
+      firestore.$getUserDatabase().where("email", "==", mentorEmail).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          var mentorObject = doc.data();
+          var result = {};
+          if (typeof mentorObject.apprentices === 'undefined' || typeof mentorObject.apprentices[apprenticeEmail] === 'undefined') {
+            result.status = "unrequested";
+          } else {
+            if (mentorObject.apprentices[apprenticeEmail]) {
+              result.status = "confirmed";
+            } else {
+              result.status = "unconfirmed";
+            }
+          }
+          defer.resolve(result);
+        });
+      });
+      return defer.promise;
+    };
+
+    firestore.$followApprenticeWithEmailFromMentorWithEmail = function (mentorEmail, apprenticeEmail) {
+      return firestore.$getUserDatabase().where("email", "==", mentorEmail).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          var updateUpprentices = {};
+          updateUpprentices["apprentices." + apprenticeEmail.replace(".", "###")] = false;
+          return firestore.$getUserWithId(doc.id).update(updateUpprentices);
+        });
+      });
+    };
+
+    firestore.$getConfirmedApprenticesFromMentorWithEmail = function(mentorEmail) {
+      var defer = $q.defer();
+      var confirmedApprentices = [];
+      firestore.$getUserDatabase().where("email", "==", mentorEmail).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          var apprentices = doc.data().apprentices;
+          console.log(apprentices);
+          angular.forEach(apprentices, function (confirmed, apprenticeEmail) {
+            if (confirmed) {
+              firestore.$getUserWithEmail(apprenticeEmail.replace("###", ".")).then(function(confirmedApprentice) {
+                confirmedApprentices.push(confirmedApprentice);
+              });
+            }
+          });
+          defer.resolve(confirmedApprentices);
+        });
+      });
+      return defer.promise;
+    };
+
+    firestore.$getUnconfirmedApprenticesFromMentorWithEmail = function(mentorEmail) {
+      var defer = $q.defer();
+      var unconfirmedApprentices = [];
+      firestore.$getUserDatabase().where("email", "==", mentorEmail).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          var apprentices = doc.data().apprentices;
+          console.log(apprentices);
+          angular.forEach(apprentices, function (confirmed, apprenticeEmail) {
+            if (!confirmed) {
+              firestore.$getUserWithEmail(apprenticeEmail.replace("###", ".")).then(function(unconfirmedApprentice) {
+                unconfirmedApprentices.push(unconfirmedApprentice);
+              });
+            }
+          });
+          console.log(unconfirmedApprentices);
+          defer.resolve(unconfirmedApprentices);
+        });
+      });
+      return defer.promise;
+    };
+
+    firestore.$getUnconfirmedMentorsFromApprenticeWithEmail = function(apprenticeEmail) {
+      return firestore.$getUserDatabase().where("apprentices." + apprenticeEmail.replace(".", "###"), "==", false).get();
+    };
+
+    firestore.$getConfirmedMentorsFromApprenticeWithEmail = function(apprenticeEmail) {
+      return firestore.$getUserDatabase().where("apprentices." + apprenticeEmail.replace(".", "###"), "==", true).get();
+    };
+
+    firestore.$acceptMentorWithEmailFromApprenticeWithEmail = function (mentorEmail, apprenticeEmail) {
+      return firestore.$getUserDatabase().where("email", "==", mentorEmail).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          var updateUpprentices = {};
+          updateUpprentices["apprentices." + apprenticeEmail.replace(".", "###")] = true;
+          return firestore.$getUserWithId(doc.id).update(updateUpprentices);
+        });
+      });
+    };
+
+    firestore.$getJournalDatabase = function () {
       return firestore.collection('journals');
     };
 
-    firestore.$createJournal = function(title, date, text, author) {
+    firestore.$createJournal = function (title, date, text, author) {
       return firestore.$getJournalDatabase().add({
         title: title,
         date: date,
@@ -50,11 +175,11 @@ angular.module('lejour.firebase.firestore', [
       return firestore.$getJournalWithId(id).get();
     };
 
-    firestore.$getJournalsWithEmail = function(email) {
+    firestore.$getJournalsWithEmail = function (email) {
       return firestore.$getJournalDatabase().where("author", "==", email).get();
     };
 
-    firestore.$updateJournalWithId = function(id, title, date, text, author) {
+    firestore.$updateJournalWithId = function (id, title, date, text, author) {
       return firestore.$getJournalWithId(id).set({
         title: title,
         date: date,
@@ -63,22 +188,22 @@ angular.module('lejour.firebase.firestore', [
       })
     };
 
-    firestore.$deleteJournalWithId = function(id) {
+    firestore.$deleteJournalWithId = function (id) {
       return firestore.$getJournalWithId(id).delete();
     };
 
-    firestore.$getCommentDatabaseFromJournalWithId = function(id) {
+    firestore.$getCommentDatabaseFromJournalWithId = function (id) {
       return firestore.$getJournalWithId(id).collection('comments');
     };
 
-    firestore.$addCommentToJournalWithId = function(id, author, text) {
+    firestore.$addCommentToJournalWithId = function (id, author, text) {
       return firestore.$getCommentDatabaseFromJournalWithId(id).add({
         author: author,
         text: text
       })
     };
 
-    firestore.$getCommentsFromJournalWithId = function(id) {
+    firestore.$getCommentsFromJournalWithId = function (id) {
       return firestore.$getCommentDatabaseFromJournalWithId(id).get();
     };
 
